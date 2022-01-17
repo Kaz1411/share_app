7c2f53b6c2afbabe9b80a084b71ec1347f32b1b4
Kaz1411 <mad0908141@gmail.com>
2022-01-17 21:07:43 +0900

    予約機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'home#top'
   devise_for :users, :controllers => {
     :registrations => 'users/registrations',
@@ -8,10 +7,9 @@ Rails.application.routes.draw do
   devise_scope :users do
     get "sign_in", :to => "users/sessions#new"
     get "sign_out", :to => "users/sessions#destroy"
-
   end
+
   get 'users/show'
-  get 'reservations/index'
   resources :rooms
   resources :reservations
 end
